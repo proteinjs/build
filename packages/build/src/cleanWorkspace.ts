465713b77a4970f69abedf17f52cb046ae5acdbc
Brent Bahry <brentbahry@gmail.com>
2024-04-17 13:24:32 -0700

    added `clean-workspace`

---
+++ packages/build/src/cleanWorkspace.ts
@@ -0,0 +1,18 @@
+import * as path from 'path'
+import { PackageUtil, cmd, WorkspaceMetadata } from '@proteinjs/util-node'
+import { Logger } from '@proteinjs/util'
+
+export const cleanWorkspace = async (workspaceMetadata?: WorkspaceMetadata) => {
+  const logger = new Logger('cleanWorkspace');
+  const workspacePath = process.cwd();
+  const { packageMap, sortedPackageNames } = workspaceMetadata ? workspaceMetadata : await PackageUtil.getWorkspaceMetadata(workspacePath);
+  const filteredPackageNames = sortedPackageNames.filter(packageName => !!packageMap[packageName].packageJson.scripts?.clean && packageName != 'typescript-parser');
+
+  logger.info(`> Cleaning ${filteredPackageNames.length} package${filteredPackageNames.length != 1 ? 's' : ''} in workspace (${workspacePath})`);
+  for (let packageName of filteredPackageNames) {
+    const localPackage = packageMap[packageName];
+    const packageDir = path.dirname(localPackage.filePath);
+    await cmd('npm', ['run', 'clean'], { cwd: packageDir });
+  }
+  logger.info(`> Finished cleaning ${filteredPackageNames.length} package${filteredPackageNames.length != 1 ? 's' : ''} in workspace (${workspacePath})`);
+}
---
+++ packages/build/src/runCleanWorkspace.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { cleanWorkspace } from './cleanWorkspace'
+
+cleanWorkspace();
