2e39ababfc9bda9db73acbb76c7d7aeeadf796b0
Brent Bahry <brentbahry@gmail.com>
2024-05-02 14:31:21 -0700

    fix: `workspacePackageCommand` needs to re-build packageMap after executing npm command

--- packages/build/src/workspacePackageCommand.ts
+++ packages/build/src/workspacePackageCommand.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 import { LogColorWrapper, PackageUtil, cmd } from '@proteinjs/util-node'
 import { Logger } from '@proteinjs/util'
-import { primaryLogColor } from './logColors'
+import { primaryLogColor, secondaryLogColor } from './logColors'
 
 /**
  * Run a command in the directory of the specified package.
@@ -14,15 +14,22 @@ export const workspacePackageCommand = async () => {
   const command = process.argv[3];
   const args = process.argv.slice(4);
   const cw = new LogColorWrapper();
-  const logger = new Logger(`workspace-package(${cw.color(packageName, primaryLogColor)}):${command}`);
+  const logger = new Logger(`${cw.color('workspace-package(', primaryLogColor)}${cw.color(packageName, secondaryLogColor)}${cw.color(')', primaryLogColor)}`);
   const workspacePath = process.cwd();
   const { packageMap } = await PackageUtil.getWorkspaceMetadata(workspacePath);
   const localPackage = packageMap[packageName];
   if (!localPackage)
-    throw new Error(`Package (${cw.color(packageName, primaryLogColor)}) does not exist in workspace: ${workspacePath}`)
+    throw new Error(`Package (${cw.color(packageName, secondaryLogColor)}) does not exist in workspace: ${workspacePath}`)
   
   const packageDir = path.dirname(localPackage.filePath);
-  await cmd(command, args, { cwd: packageDir }, { logPrefix: `[${cw.color(packageName, primaryLogColor)}] ` });
-  if (command === 'npm')
+  logger.info(`Running command: ${cw.color(`${command} ${args.join(' ')}`, secondaryLogColor)}`);
+  await cmd(command, args, { cwd: packageDir }, { logPrefix: `[${cw.color(packageName, secondaryLogColor)}] ` });
+  logger.info(`Finished running command: ${cw.color(`${command} ${args.join(' ')}`, secondaryLogColor)}`);
+  if (command === 'npm') {
+    logger.info(`Symlinking local dependencies`);
+    const { packageMap } = await PackageUtil.getWorkspaceMetadata(workspacePath);
+    const localPackage = packageMap[packageName];
     await PackageUtil.symlinkDependencies(localPackage, packageMap, logger);
+    logger.info(`Symlinked local dependencies`);
+  }
 }
